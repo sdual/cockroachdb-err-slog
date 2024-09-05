c7c79bee44385eae17b740b6fd003654626ba2c0
sdual <shr.nrht.0623@gmail.com>
2024-09-05 22:36:53 +0900

    add roach slog handler

--- handler.go
+++ handler.go
@@ -1,7 +1,91 @@
 package roachslog
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
 
-type RoachSlogHandler struct {
-	handler slog.Handler
+	"github.com/cockroachdb/errors"
+)
+
+const (
+	defaultErrorAttrKey     = "error"
+	defaultStactraceAttrKey = "stacktrace"
+)
+
+type (
+	RoachSlogHandler struct {
+		handler       slog.Handler
+		errAttrKey    string
+		stacktraceKey string
+	}
+
+	opt func(*RoachSlogHandler)
+)
+
+// NewReachSlogHandler creates a new RoachSlogHandler instance.
+func NewReachSlogHandler(handler slog.Handler, opts ...opt) RoachSlogHandler {
+
+	rs := &RoachSlogHandler{
+		handler:       handler,
+		errAttrKey:    defaultErrorAttrKey,
+		stacktraceKey: defaultStactraceAttrKey,
+	}
+
+	for _, option := range opts {
+		option(rs)
+	}
+	return *rs
+}
+
+func WithErrorAttrKey(key string) opt {
+	return func(rs *RoachSlogHandler) {
+		rs.errAttrKey = key
+	}
+}
+
+func WithStacktraceAttrKey(key string) opt {
+	return func(rs *RoachSlogHandler) {
+		rs.stacktraceKey = key
+	}
+}
+
+func (rs *RoachSlogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return rs.handler.Enabled(ctx, level)
+}
+
+func (rs *RoachSlogHandler) Handle(ctx context.Context, record slog.Record) error {
+	var stacktrace string
+	record.Attrs(func(attr slog.Attr) bool {
+		if attr.Key == rs.errAttrKey {
+			err, ok := attr.Value.Any().(error)
+			// If the cast to type error fails, no information about the error is logged.
+			// The value of the error data type should be available as the value corresponding to the specified key (errAttrKey).
+			if !ok {
+				return false
+			}
+			stacktrace, ok = extractStacktrace(err)
+			if !ok {
+				return false
+			}
+		}
+		return true
+	})
+
+	record.AddAttrs(slog.String(rs.stacktraceKey, stacktrace))
+	return rs.handler.Handle(ctx, record)
+}
+
+func (rs *RoachSlogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &RoachSlogHandler{handler: rs.handler.WithAttrs(attrs)}
+}
+func (rs *RoachSlogHandler) WithGroup(g string) slog.Handler {
+	return &RoachSlogHandler{handler: rs.handler.WithGroup(g)}
+}
+
+func extractStacktrace(err error) (string, bool) {
+	safeDetails := errors.GetSafeDetails(err).SafeDetails
+	if len(safeDetails) > 0 {
+		return safeDetails[0], true
+	}
+	return "", false
 }
